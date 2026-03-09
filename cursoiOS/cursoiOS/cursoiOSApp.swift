2ebd158a5941896f56f694f07be956d96871708d
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-03-08 20:48:01 -0500

    testing new app

---
+++ cursoiOS/cursoiOS/cursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursoiOSApp.swift
+//  cursoiOS
+//
+//  Created by Camilo on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct cursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
